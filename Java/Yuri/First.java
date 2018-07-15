fbd334572e677303420699dca101a53be19a0445
jiham7 <jiham7@gmail.com>
2018-07-15 16:05:38 -0500

    Yuri lesson 1

---
+++ Java/Yuri/First.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class First here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class First
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class First
+     */
+    public First()
+    {
+        // initialise instance variables
+        x = 5;
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int addToX(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ Java/Yuri/HelloWorldApp.java
@@ -0,0 +1,49 @@
+class HelloWorldApp {
+    public static void main(String[] args) {
+        // int number = 10;
+        // int number2 = number;
+        // number = number2 * 2;
+        // number2 = number + 3;
+        // number = number + number2;
+        
+        // System.out.println(number);
+        // System.out.println(number2);
+        
+
+        String words = "Blah blah.";
+        String words2 = "yellow";
+        String combined = words + words2;
+        
+        int[] list = {1,2,3,6,10,76,4,5};
+        
+        
+        // for(int i = 0; i < 6; i++)
+        // {
+            // System.out.println("hello");
+        // }
+        
+        // for(int i = 10; i >= 6; i--)
+        // {
+            // System.out.println("hello");
+        // }
+        
+        // for(int i = 10; i >= 6; i = i+2)
+        // {
+            // System.out.println("hello");
+        // }
+        
+        // for(int a = 0; a < list.length; a = a +3)
+        // {
+            // System.out.println(list[a]);
+        // }
+        
+        int a = 0;
+        while(a <6) {
+            System.out.println("hello");
+            a++;
+        }
+        
+        
+        // System.out.println(combined);
+    }
+}
